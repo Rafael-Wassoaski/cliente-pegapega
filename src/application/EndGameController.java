d6235e25be826744ae31ba64d7ca066a39e42278
Rafael-Wassoaski <meireles200@hotmail.com>
2019-11-06 11:02:14 -0300

    Update EndGameController.java

--- src/application/EndGameController.java
+++ src/application/EndGameController.java
@@ -15,6 +15,7 @@ public class EndGameController implements Initializable{
 	@FXML
 	Text texto;
 	public void setComo(String como) {
+		//função que colocar como o game acabou na tela de endgame
 		texto.setText(como);
 	}
 	@Override
