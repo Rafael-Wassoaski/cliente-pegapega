178cd39efb6aafd788a32b32fa3d2a306a85f736
Rafael-Wassoaski <meireles200@hotmail.com>
2019-11-06 10:54:29 -0300

    Update TelaGameController.java

--- src/application/TelaGameController.java
+++ src/application/TelaGameController.java
@@ -11,7 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class TelaGameController implements Initializable {
-
+//a thread de envio pega o tempo;x;y  daqui, esse é o game msm
 	public static int x, y;
 	public static float tempoFloat;
 	public static boolean game = true;
@@ -23,7 +23,7 @@ public class TelaGameController implements Initializable {
 	Text aviso;
 	@FXML
 	Button cima, baixo, esquerda, direita;
-	
+	//função que é disparada pela thread de recebimento quando recebe um "Próximo"
 	public void setAviso(String msg, float tempo) {
 		
 		aviso.setText("No tempo " + tempo +" "+msg);
@@ -32,7 +32,7 @@ public class TelaGameController implements Initializable {
 	}
 	
 	private void block() {
-		
+		//função que trava os botões e libera eles dps de 1 segundo, feita pra o jogardor não mandar mt requisição
 		cima.setDisable(true);
 		baixo.setDisable(true);
 		esquerda.setDisable(true);
@@ -40,7 +40,7 @@ public class TelaGameController implements Initializable {
 		
 		try {
 			Thread.currentThread().sleep(1000);
-			
+			//habilita os tões
 			cima.setDisable(false);
 			baixo.setDisable(false);
 			esquerda.setDisable(false);
@@ -55,6 +55,8 @@ public class TelaGameController implements Initializable {
 	}
 	
 	private void setCampo() {
+		//funçãoq que é chamada sempre que um botão (cima baixo por ai vai) é apertado, 
+		//essa função refaz o campo, 2 for , se o for bater estiver no msm valor que x e y ele coloca 1, representando o jogador
 		String campoUnificado = "";
 		for(int a = 0; a < 8 ;a ++) {
 			for(int b = 0; b < 8 ;b ++) {
@@ -67,23 +69,25 @@ public class TelaGameController implements Initializable {
 			campoUnificado+="\n";
 		}
 		
+		//coloca o campo pronto no textfield
 		campo.setText(campoUnificado);
 		
 	}
 	
 	public void atualizaTempo() {
+		//função chamada na thread recebimento quando chegar a msg de novo tempo, ele chama aqui e atualiza ele na interface
 		tempo.setText(Float.toString(tempoFloat));
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+		//inicia as variáveis 
 		x = y = 0;
 		tempoFloat = 0;
 		
 		setCampo();
-		
+		//cada um desses é uma ação de botão
 		cima.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
